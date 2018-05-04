dcd6fc117b98e2370d69836f6e60def6b7026d7e
GeniusV <eliot310100@gmail.com>
2018-05-04 17:22:42 +0800

    prime ok string count ok setup ok modify gitignore

--- exp-3-1520969HuaJiacheng/exp-3/prime.py
+++ exp-3-1520969HuaJiacheng/exp-3/prime.py
@@ -3,11 +3,13 @@
 
 
 def is_prime(n: int):
+    if n < 2:
+        raise ArithmeticError("prime must >= 2")
     for i in range(2, n):
         if n % i == 0:
             return False
     return True
 
 if __name__ == '__main__':
-    for i in range(16):
+    for i in range(2, 16):
         print("{} is prime? {}".format(i, is_prime(i)))

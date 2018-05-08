5d45c8e8c9b8acdc17019c9e876fb29fa0ac6942
Lee <1371179322@qq.com>
2018-05-09 00:22:50 +0800

    upload

--- 1520932 鲍昌平/1520932鲍昌平实验一/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_packages
-setup(
-    name='myapp',        
-    version='1.0',
-    packages = find_packages(),
-    py_modules=['1520932鲍昌平 素数','1520932鲍昌平 统计']
-)

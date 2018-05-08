8ab5bbd5f9b0fe384e7ca88d675f66b1c75a3313
Lee <1371179322@qq.com>
2018-05-08 23:49:40 +0800

    upload

--- 1521010杨波 汉诺塔.py
+++
@@ -1,9 +0,0 @@
-n = int(input("输入初始化时A塔上盘子的个数n：\n"))  
-def move( n , A , B ,C ):     
-    if n == 1 :         
-        print( "%s is moved to %s" %(A ,C) )  
-    else :  
-        move( n-1 , A , C , B )     
-        move( 1 , A , B , C )  
-        move( n-1 , B , A , C )  
-move( n ,'A' , 'B' , 'C')     

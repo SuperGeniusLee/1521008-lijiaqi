acec1280a895b82075142b9c5da19baf5d71342d
GeniusV <eliot310100@gmail.com>
2018-06-14 16:48:12 +0800

    add db init method in run.py

--- BigHomework/run.py
+++ BigHomework/run.py
@@ -1,8 +1,15 @@
-
 from BigHomework import create_app, db
 
 app = create_app()
 
+
+def init():
+    init_app = create_app()
+    app_context = init_app.app_context()
+    app_context.push()
+    db.create_all()
+
+
 if __name__ == '__main__':
+    init()
     app.run(debug = True, port = 8080)
-    # pass

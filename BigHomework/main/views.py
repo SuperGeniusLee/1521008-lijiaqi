1547cc1cf1766320666e5be8b5741abe656a5a61
GeniusV <eliot310100@gmail.com>
2018-06-14 16:18:01 +0800

    into post done

--- BigHomework/main/views.py
+++ BigHomework/main/views.py
@@ -2,14 +2,16 @@
 # -*-encoding: utf-8-*-
 
 # Created by GeniusV on 6/13/18.
+
 from BigHomework import db
+from flask import flash
 from flask import redirect
 from flask import render_template
 from flask import request
 from flask import url_for
 from flask.ext.login import current_user, login_required
-from main.forms import PostForm
-from models import Post
+from main.forms import PostForm, CommentForm
+from models import Post, Comment
 
 from . import main
 
@@ -46,5 +48,22 @@ def edit(id):
 
 @main.route('/post/<int:id>', methods=['GET', 'POST'])
 def post(id):
-    # todo
-    return redirect(url_for("main.index"))
+    post = Post.query.get_or_404(id)
+    form = CommentForm()
+    if form.validate_on_submit():
+        comment = Comment(body=form.body.data,
+                          post=post,
+                          author=current_user._get_current_object())
+        db.session.add(comment)
+        flash('Your comment has been published.')
+        return redirect(url_for('.post', id=post.id, page=-1))
+    page = request.args.get('page', 1, type=int)
+    if page == -1:
+        page = (post.comments.count() - 1) / \
+            10 + 1
+    pagination = post.comments.order_by(Comment.timestamp.asc()).paginate(
+        page, per_page=10,
+        error_out=False)
+    comments = pagination.items
+    return render_template('post.html', posts=[post], form=form,
+                           comments=comments, pagination=pagination)

129c8e64e9029a7347a01e08555ff42f1cbbf5c4
GeniusV <eliot310100@gmail.com>
2018-06-14 11:22:44 +0800

    display post

--- BigHomework/main/views.py
+++ BigHomework/main/views.py
@@ -3,12 +3,11 @@
 
 # Created by GeniusV on 6/13/18.
 from BigHomework import db
-from flask import current_app
 from flask import redirect
 from flask import render_template
 from flask import request
 from flask import url_for
-from flask.ext.login import current_user
+from flask.ext.login import current_user, login_required
 from main.forms import PostForm
 from models import Post
 
@@ -25,8 +24,26 @@ def index():
         return redirect(url_for('.index'))
     page = request.args.get('page', 1, type = int)
     pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
-        page, per_page = current_app.config['FLASKY_POSTS_PER_PAGE'],
+        page, per_page = 10,
         error_out = False)
     posts = pagination.items
+    return render_template('index.html', form = form, posts = posts, pagination = pagination)
 
-    return render_template('index.html')
+
+@main.route('/user/<username>')
+def user(username):
+    # todo
+    return redirect(url_for("main.index"))
+
+
+@main.route('/edit/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit(id):
+    # todo
+    return redirect(url_for("main.index"))
+
+
+@main.route('/post/<int:id>', methods=['GET', 'POST'])
+def post(id):
+    # todo
+    return redirect(url_for("main.index"))
